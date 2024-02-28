973158e49b74d0270ce40029654f7e7b5e3f2888
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-02-28 14:00:07 +0700

    update

--- autoDeploy.sh
+++ autoDeploy.sh
@@ -3,18 +3,22 @@ log_file="./deploy.log"
 log_time=$(date +"%Y-%m-%d %H:%M:%S")
 
 git fetch origin
+wait
 
 CHANGES=$(git log HEAD..origin/main --oneline)
 if [ $CHANGES ]; then
-    git pull origin main
     pm2 stop nuxt 
-    pm2 delete nuxt 
+    wait
+    git pull origin main
     yarn install
+    wait
     yarn build
+    wait
     PORT=3000 pm2 start yarn --name "nuxt" -- start
+    wait
     log_content="Deploy thành công"
 else 
-    log_content="Deploy thành công"
+    log_content="Không có gì thay đổi"
 fi
 
 echo "$log_time: $log_content" >> "$log_file"
