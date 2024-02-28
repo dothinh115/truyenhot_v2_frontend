39d600e6fffb308048b796b80274c8b77d4d1721
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-02-28 17:23:17 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
   <div
     class="flex w-screen h-screen justify-center items-center 2xl:text-[52px] xl:text-[2rem] lg:text-[1.5rem] text-center"
   >
-    Bọn mình đang chỉnh sửa 1 số thứ, hãy kiên nhẫn chờ trong vài ngày nhé :D
+    Bọn mình đang chỉnh sửa 1 số thứ, hãy kiên nhẫn chờ trong vài ngày nhé :D :D
   </div>
 </template>
