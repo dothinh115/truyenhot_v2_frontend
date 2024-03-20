82013b9fbdbb5ace66d6f2e57151e5bf0161b05d
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-03-21 02:31:15 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
-
   tailwindcss: {
     cssPath: "@/assets/scss/global.scss",
     configPath: "tailwind.config.js",
