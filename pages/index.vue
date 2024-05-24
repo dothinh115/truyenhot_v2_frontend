db4bab362855437a94a6ed64f91e7804a32c3c88
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-05-24 15:52:20 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,5 @@
     class="flex w-screen h-screen justify-center items-center 2xl:text-[52px] xl:text-[2rem] lg:text-[1.5rem] text-center"
   >
     Bọn mình đang chỉnh sửa 1 số thứ, hãy kiên nhẫn chờ trong vài ngày nhé :D :D
-    :D
   </div>
 </template>
