5afca9cb0cebf5f2fa3a5d0b5d1f80c786ba78a2
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-09-04 22:19:45 +0700

    update

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,11 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { user } = useAuth();
-
   if (!user.value) {
     const host = useRequestHeaders()["host"];
     const proto = useRequestHeaders()["x-forwarded-proto"];
     const url = `${proto}://${host}${to.fullPath}`;
-    return navigateTo(`https://cp.truyenhot.info/login?next=${url}`, {
+    return navigateTo(`http://localhost:3001/login?next=${url}`, {
       external: true,
     });
   }
