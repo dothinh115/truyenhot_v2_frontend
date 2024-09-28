b14bad1c728085ce8009c3ad799ca58349baffa6
dothinh115 <111167728+dothinh115@users.noreply.github.com>
2024-09-29 01:23:04 +0700

    update

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,13 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const { user } = useAuth();
   const { cpPath } = useRuntimeConfig().public;
+  console.log(user.value);
   if (!user.value) {
     const host = useRequestHeaders()["host"];
     const proto = useRequestHeaders()["x-forwarded-proto"];
     const url = `${cpPath}/login?next=${proto}://${host}${to.fullPath}`;
-    return navigateTo(url, {
-      external: true,
-    });
+    // return navigateTo(url, {
+    //   external: true,
+    // });
   }
 });
